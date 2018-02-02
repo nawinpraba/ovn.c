ac1b1ef5cc00de6bcb3c4e75aff0f12775093373
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-02 16:09:25 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+printf("enter the value of n");
+scanf("%d",&n);
+for(i=o,n!=0,i++);
+{
+a/=10;
+}
+printf("%d number of digits",i);
+getch();
+}
